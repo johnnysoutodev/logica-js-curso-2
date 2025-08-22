2e35047e9d80d881e29376858703f58bec66953a
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-08-22 07:03:48 -0300

    criada a função verificarChute()

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,31 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// Função com parametros e sem retorno
+function setTexto(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+// Função sem parametros
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        setTexto('h1', 'Parabéns!');
+        setTexto('p', 'Você acertou o número secreto!');
+    } else {
+        if(chute > numeroSecreto){
+            setTexto('p', 'Você errou. O número secreto é menor.');
+        } else {
+            setTexto('p', 'Você errou. O número secreto é maior.');
+        }
+    }
+}
+
+// Função que retorna um valor
+function gerarNumeroAleatorio(){
+    return parseInt(Math.floor(Math.random() * 10) + 1);
+}
+
+setTexto('h1', 'Jogo do número secreto');
+setTexto('p', 'Escolha um número entre 1 e 10');
