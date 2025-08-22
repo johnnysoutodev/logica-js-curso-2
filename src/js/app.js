ef2b4a9bf6ee3d019c1df95b6a3063ab35863ef8
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-08-22 05:44:16 -0300

    criada a variavel do paragrafo

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do número secreto';
-console.log(titulo.innerHTML);
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
